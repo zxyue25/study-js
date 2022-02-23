428a1983373dc64756d14517029e719d4d4924e7
zhengxiuyue <zhengxiuyue@jd.com>
2022-02-23 20:08:56 +0800

    feat: promise1.0

---
+++ promise/promise1.js
@@ -0,0 +1,57 @@
+const PENDING = 'pending'
+const RESOLVED = 'resolved'
+const REJECTED = 'rejected'
+
+class Promise {
+    constructor(executor) {
+        this.status = PENDING
+        this.value = undefined // 成功的值
+        this.reason = undefined // 失败原因
+
+        // 处理异步
+        this.onFulfilledCallbacks = []
+        this.onRejectCallbacks = []
+
+        // 成功函数
+        let resolve = (value) => {
+            if (this.status === PENDING) {
+                this.value = value
+                this.status = RESOLVED
+                this.onFulfilledCallbacks.forEach(fn => fn());
+            }
+        }
+
+        // 失败函数
+        let reject = (reason) => {
+            if (this.status === PENDING) {
+                this.reason = reason
+                this.status = REJECTED
+                this.onRejectCallbacks.forEach(fn => fn())
+            }
+        }
+
+        try {
+            executor(resolve, reject) // 默认执行器立即执行
+        }
+        catch (e) {
+            reject(e) // 如果立即执行函数发生错误等价于调用失败函数
+        }
+    }
+    then (onFulfilled, onReject) {
+        // 同步
+        if (this.status === RESOLVED) {
+            onFulfilled(this.value)
+        }
+        if (this.status === REJECTED) {
+            onReject(this.reason)
+        }
+
+        // 异步订阅
+        if (this.status === PENDING) { 
+            this.onFulfilledCallbacks.push(() => onFulfilled(this.value))
+            this.onRejectCallbacks.push(() => onReject(this.reason))
+        }
+    }
+}
+
+module.exports = Promise
---
+++ promise/test1.js
@@ -0,0 +1,31 @@
+const Promise = require('./promise1')
+
+const promise = new Promise((resolve, reject) => {
+    // 成功的情况
+    // resolve(1)
+    // 失败的情况
+    // reject(1)
+    // 异步的情况
+    setTimeout(() => {
+        reject(1)
+    }, 1000)
+
+})
+
+promise.then(data => {
+    console.log(data)
+}, err => {
+    console.log('err', err)
+})
+
+promise.then(data => {
+    console.log(data)
+}, err => {
+    console.log('err', err)
+})
+
+promise.then(data => {
+    console.log(data)
+}, err => {
+    console.log('err', err)
+})
